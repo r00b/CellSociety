a5a256a1e9907c9e8cbd05e120df6a6d6c04b478
Robert Steilberg <rsteilberg@gmail.com>
2016-09-22 12:41:52 -0400

    implement speed slider functionality

--- src/gui/Animation.java
+++ src/gui/Animation.java
@@ -6,6 +6,7 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Slider;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -13,11 +14,12 @@ import javafx.util.Duration;
 
 public class Animation {
 	private static final String TITLE = "CellSociety";
-	public static final int WIDTH = 800;
-	public static final int HEIGHT = 600;
-	public static final int FRAMES_PER_SECOND = 60;
-	public static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
-	public static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
+	private static final int WIDTH = 800;
+	private static final int HEIGHT = 600;
+	private double DEFAULT_FPS = 5;
+	private double DEFAULT_MILLISECOND_DELAY = 1000 / DEFAULT_FPS;
+	private double DEFAULT_SECOND_DELAY = 1.0 / DEFAULT_FPS;
+	int myTest = 1;
 
 	/**
 	 * Get the window title for the scene
@@ -27,44 +29,74 @@ public class Animation {
 	public String getTitle() {
 		return TITLE;
 	}
-	
+
+	/**
+	 * Handle user inputs to the speed slider and change the simulation
+	 * step speed
+	 * 
+	 * @param e
+	 *            the MouseEvent registering the click
+	 * @param t
+	 *            the Timeline that is running through the step function
+	 */
+	private void handleSlider(Node n, Timeline t) {
+		t.stop(); // stop the current run
+		t.getKeyFrames().clear(); // clear out the old Timeline with old speed
+		Slider speedSlider = (Slider) n;
+		double framesPerSecond = speedSlider.getValue() / 5;
+		double millisecondDelay = 1000 / framesPerSecond;
+		double secondDelay = 1.0 / framesPerSecond;
+		KeyFrame k = new KeyFrame(Duration.millis(millisecondDelay), e -> step(secondDelay));
+		t.getKeyFrames().add(k);
+		t.play(); // resume simulation
+	}
+
 	/**
 	 * Handle click to pause button by pausing the simulation
 	 * 
-	 * @param e the MouseEvent registering the click
-	 * @param t the Timeline that is running through the step function
+	 * @param e
+	 *            the MouseEvent registering the click
+	 * @param t
+	 *            the Timeline that is running through the step function
 	 */
 	private void handlePause(MouseEvent e, Timeline t) {
 		t.pause();
 	}
-	
+
 	/**
 	 * Handle click to stop button by stopping the simulation
 	 * 
-	 * @param e the MouseEvent registering the click
-	 * @param t the Timeline that is running through the step function
+	 * @param e
+	 *            the MouseEvent registering the click
+	 * @param t
+	 *            the Timeline that is running through the step function
 	 */
 	private void handleStop(MouseEvent e, Timeline t) {
 		t.stop();
 	}
-	
+
 	/**
-	 * Handle click to step button by stepping through one cycle of the simulation
+	 * Handle click to step button by stepping through one cycle of the
+	 * simulation
 	 * 
-	 * @param e the MouseEvent registering the click
-	 * @param t the Timeline that is running through the step function
+	 * @param e
+	 *            the MouseEvent registering the click
+	 * @param t
+	 *            the Timeline that is running through the step function
 	 */
 	private void handleStep(MouseEvent e, Timeline t) {
 		t.pause(); // in case we are already playing
 		t.play();
 		t.pause();
 	}
-	
+
 	/**
 	 * Handle click to play button by starting the simulation
 	 * 
-	 * @param e the MouseEvent registering the click
-	 * @param t the Timeline that is running through the step function
+	 * @param e
+	 *            the MouseEvent registering the click
+	 * @param t
+	 *            the Timeline that is running through the step function
 	 */
 	private void handlePlay(MouseEvent e, Timeline t) {
 		t.play();
@@ -75,7 +107,7 @@ public class Animation {
 	 */
 	private Timeline initStep() {
 		Timeline t = new Timeline();
-		KeyFrame frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY), e -> step(SECOND_DELAY));
+		KeyFrame frame = new KeyFrame(Duration.millis(DEFAULT_MILLISECOND_DELAY), e -> step(DEFAULT_SECOND_DELAY));
 		t = new Timeline();
 		t.setCycleCount(Timeline.INDEFINITE);
 		t.getKeyFrames().add(frame);
@@ -102,13 +134,15 @@ public class Animation {
 	public Scene init() {
 		Controls controllers = new Controls();
 		Pane root = controllers.getControlPane();
-		HashMap<String,Node> nodes = controllers.getControls(root);
+		HashMap<String, Node> nodes = controllers.getControls(root);
 		Scene simulation = new Scene(root, WIDTH, HEIGHT, Color.GRAY);
 		Timeline t = initStep();
-		nodes.get("PLAY").setOnMouseClicked(e -> handlePlay(e,t));
-		nodes.get("STEP").setOnMouseClicked(e -> handleStep(e,t));
-		nodes.get("PAUSE").setOnMouseClicked(e -> handlePause(e,t));
-		nodes.get("STOP & RESET").setOnMouseClicked(e -> handleStop(e,t));
+		nodes.get("PLAY").setOnMouseClicked(e -> handlePlay(e, t));
+		nodes.get("STEP").setOnMouseClicked(e -> handleStep(e, t));
+		nodes.get("PAUSE").setOnMouseClicked(e -> handlePause(e, t));
+		nodes.get("STOP & RESET").setOnMouseClicked(e -> handleStop(e, t));
+		nodes.get("slider").setOnMouseDragged(e -> handleSlider(nodes.get("slider"), t));
+		nodes.get("slider").setOnKeyPressed(e -> handleSlider(nodes.get("slider"), t));
 		return simulation;
 	}
 }
