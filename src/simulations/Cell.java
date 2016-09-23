5c481b2ccace5865fa1fbda4a8373d70126c439b
Sam Curtis <sjc43@duke.edu>
2016-09-23 03:52:56 -0400

    Finished game of life simulation class

--- src/simulations/Cell.java
+++ src/simulations/Cell.java
@@ -6,25 +6,40 @@ public class Cell {
 	private final ArrayList<Cell> neighbors;
 	private final Tuple myPosition;
 	private String currentState;
-	private final ArrayList<String> possibleStates;
+	private String nextState;
 	
 	public Cell(int i, int j){
 		myPosition = new Tuple(i,j);
 		neighbors = new ArrayList<Cell>();
-		possibleStates = new ArrayList<String>();
 	}
 	
-	public void addState(String state){
-		possibleStates.add(state);
+	public void setNextState(String next){
+		nextState = next;
+	}
+	 
+	public void commitState(){
+		currentState = nextState;
 	}
 	
+	public String getCurrState(){
+		return currentState;
+	}
+	
+	
+	public void addNeighbor(Cell neighbor){
+		neighbors.add(neighbor);
+	}
 	public void setCurrState(String state){
 		currentState = state;
-		
 	}
 	
-	public ArrayList<String> getPossibleStates(){
-		return possibleStates;
+	
+	public Tuple getPosition(){
+		return myPosition;
+	}
+	
+	public ArrayList<Cell> getNeighbors(){
+		return neighbors;
 	}
 	
 	
--- src/simulations/GameOfLife.java
+++ src/simulations/GameOfLife.java
@@ -1,9 +1,9 @@
 package simulations;
 
 
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.ResourceBundle;
-
 import xml.GameOfLifeXMLParser;;
 
 public class GameOfLife {
@@ -12,12 +12,15 @@ public class GameOfLife {
 	private final int probCellAlive;
 	public static final String DEFAULT_RESOURCE_PACKAGE = "resources/";
 	public static final String LANGUAGE = "English";
+	private final ArrayList<String> possibleStates;
 	
 	
 	public GameOfLife() {
 		myParser = new GameOfLifeXMLParser("../data/GameOfLife.xml");
 		probCellAlive = myParser.getProbOfCellAlive();
+		possibleStates = new ArrayList<String>();
 		myGrid = new Grid(getGridWidth(),getGridHeight());
+		setInitialGridState();
 	}
 	
 	
@@ -37,31 +40,120 @@ public class GameOfLife {
 				Cell currCell = myGrid.getCell(i, j);
 				setPossibleStates(currCell);
 				setRandomInitialState(probCellAlive, currCell);
+				addNeighbors(currCell);
 			}
 		}
 	}
 	
+	private void addNeighbors(Cell currCell) {
+		Tuple position = currCell.getPosition();
+		for(int k = -1; k<2; k++){
+			for(int g = -1; g<2; g++){
+				int i = getNeighborIPosition(position, position.getIPos(), k);
+				int j = getNeighborJPosition(position, position.getJPos(), g);
+				currCell.addNeighbor(myGrid.getCell(i, j));
+				
+			}
+		}
+		
+	}
+	
+	private int getNeighborJPosition(Tuple position, int j, int g) {
+		int jPos = position.getJPos() + g;
+		if(jPos < 0){
+			jPos = myGrid.getWidth();
+		}
+		
+		if(jPos > 0){
+			jPos = 0;
+		}
+		return jPos;
+	}
+
+
+	private int getNeighborIPosition(Tuple position,int i, int k){
+		int iPos = position.getIPos() + k;
+		if(iPos < 0){
+			iPos = myGrid.getHeight();
+		}
+		
+		if(iPos > 0){
+			iPos = 0;
+		}
+		return iPos;
+	}
+
+
 	private void setRandomInitialState(int prob_Cell_Alive, Cell currCell) {
 		Random random = new Random();
 		int randNum = random.nextInt(101);
 		if(randNum < 50){
-			currCell.setCurrState(currCell.getPossibleStates().get(0));
+			currCell.setCurrState(possibleStates.get(0));
 		}
 		else{
-			currCell.setCurrState(currCell.getPossibleStates().get(1));
+			currCell.setCurrState(possibleStates.get(1));
 		}
 		
 	}
 
 	private void setPossibleStates(Cell currCell) {
 		ResourceBundle resources = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE + LANGUAGE);
-		currCell.addState(resources.getString("Dead"));
-		currCell.addState(resources.getString("Alive"));
-		
+		possibleStates.add(resources.getString("Dead"));
+		possibleStates.add(resources.getString("Alive"));
 	}
 	
+	private void updateNextStates(){
+		for(int i = 0; i < myGrid.getHeight(); i++){
+			for(int j = 0; j<myGrid.getWidth(); j++){
+				Cell currCell = myGrid.getCell(i, j);
+				int numNeighborsAlive = calculateNumNeighborsAlive(currCell);
+				if(currCell.getCurrState() == "Dead"){
+					if(numNeighborsAlive == 3){
+						currCell.setNextState("Alive");
+						continue;
+					}
+					else{
+						currCell.setNextState("Dead");
+						continue;
+					}
+				}
+				else{
+					if(numNeighborsAlive == 2 || numNeighborsAlive == 3){
+						currCell.setNextState("Alive");
+						continue;
+					}
+					else{
+						currCell.setNextState("Dead");
+					}
+				}
+				
+			}
+		}
+	}
 	
+	private void commitStates(){
+		for(int i = 0; i < myGrid.getHeight(); i++){
+			for(int j = 0; j<myGrid.getWidth(); j++){
+				Cell currCell = myGrid.getCell(i, j);
+				currCell.commitState();
+			}
+		}
+	}
 	
+	public void updateGrid(){
+		updateNextStates();
+		commitStates();
+	}
+
+	private int calculateNumNeighborsAlive(Cell currCell) {
+		int numAlive = 0;
+		for(Cell neighbor : currCell.getNeighbors()){
+			if(neighbor.getCurrState() == "Alive"){
+				numAlive += 1;
+			}
+		}
+		return numAlive;
+	}
 	
 	
 }
