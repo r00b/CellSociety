84b8e8d351ec6ee0a2b138fc0163d6f0010bbcc6
Sam Curtis <sjc43@duke.edu>
2016-09-23 17:54:10 -0400

    Commented grid and cell classes

--- src/simulations/Cell.java
+++ src/simulations/Cell.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 
 import javafx.scene.paint.Color;
 
+/**
+ * @author samuelcurtis
+ *
+ *Cells all have a position, currentState, nextState, neighbors, and stateColor. Cells used to populate grids.
+ */
 public class Cell {
 	private final ArrayList<Cell> neighbors;
 	private final Tuple myPosition;
@@ -11,45 +16,79 @@ public class Cell {
 	private String nextState;
 	private Color stateColor;
 	
+	/**
+	 * @param i the rom number of the row this cell is in 
+	 * @param j the colum number of the column this cell is in
+	 */
 	public Cell(int i, int j){
 		myPosition = new Tuple(i,j);
 		neighbors = new ArrayList<Cell>();
 	}
 	
+	/**
+	 * @param next - the State that this cell will be in in the next generation
+	 */
 	public void setNextState(String next){
 		nextState = next;
 	}
 	 
+	/**
+	 * @param color is the color of the cells new state. This method updates the current state to the next date,
+	 * should be called once all cells have their next generation state calculated 
+	 */
 	public void commitState(Color color){
 		currentState = nextState;
 		stateColor = color;
 	}
 	
+	/**
+	 * @return the current state of this cell
+	 */
 	public String getCurrState(){
 		return currentState;
 	}
 	
+	/**
+	 * @return the color associated with the cells current state as defined by a simulation class
+	 */
 	public Color getStateColor(){
 		return stateColor;
 	}
 	
 	
+	/**
+	 * @param neighbor is a cell that neighbors this cell. this method adds that cell to the instance variable
+	 * neighbors, which stores all of the cells neighboring cells
+	 */
 	public void addNeighbor(Cell neighbor){
 		neighbors.add(neighbor);
 	}
+	/**
+	 * @param state is the state the cell will be set to next 
+	 * @param color is the color associated with that state 
+	 */
 	public void setCurrState(String state, Color color){
 		currentState = state;
 		stateColor = color;
 	}
 	
+	/**
+	 * @return the state the cell will be in next generation 
+	 */
 	public String getNextState(){
 		return nextState;
 	}
 	
+	/**
+	 * @return the tuple representing the (i,j) position of the cell within a grid
+	 */
 	public Tuple getPosition(){
 		return myPosition;
 	}
 	
+	/**
+	 * @return an arraylist of the cells neighboring cells 
+	 */
 	public ArrayList<Cell> getNeighbors(){
 		return neighbors;
 	}
--- src/simulations/Grid.java
+++ src/simulations/Grid.java
@@ -1,16 +1,28 @@
 package simulations;
 
+/**
+ * @author samuelcurtis
+ *
+ *Used to represent a grid of cells. Used by each simulation class to provide a template for the cells interactions.
+ */
 public class Grid {
 	private final int gridWidth;
 	private final int gridHeight;
 	private Cell[][] grid;
 	
+	/**
+	 * @param width is the number of columns in the grid
+	 * @param Height is the number of rows in the grid
+	 */
 	public Grid(int width, int Height){
 		gridWidth = width;
 		gridHeight = Height;
 		initializeGrid();
 	}
 
+	/**
+	 * Populate the grid with cells, tell each cell what its position within the grid is upon initialization
+	 */
 	private void initializeGrid() {
 		grid = new Cell[gridHeight][gridWidth];
 		for(int i = 0; i < gridHeight; i++){
@@ -22,14 +34,25 @@ public class Grid {
 		
 	}
 	
+	/**
+	 * @param i - the row number of the cell dsired
+	 * @param j - the column number of the cell desired
+	 * @return the cell at position (i,j)
+	 */
 	public Cell getCell(int i, int j){
 		return grid[i][j];
 	}
 	
+	/**
+	 * @return the width (the number of columns) in the grid
+	 */
 	public int getWidth(){
 		return gridWidth;
 	}
 	
+	/**
+	 * @return the height (the number of rows) in the grid
+	 */
 	public int getHeight(){
 		return gridHeight;
 	}
