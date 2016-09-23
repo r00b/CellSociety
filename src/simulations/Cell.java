86b5dbfb2c871d4e48c0e90cd095f3934c0fec8c
Sam Curtis <sjc43@duke.edu>
2016-09-22 23:24:05 -0400

    Add custom tuple class, add instance variables neighbors, myPosition, currentState and constructor to cell class

--- src/simulations/Cell.java
+++ src/simulations/Cell.java
@@ -1,6 +1,17 @@
 package simulations;
 
+import java.util.ArrayList;
+
 public class Cell {
+	private final ArrayList<Cell> neighbors;
+	private final Tuple myPosition;
+	private String currentState;
+	
+	public Cell(int i, int j){
+		myPosition = new Tuple(i,j);
+		neighbors = new ArrayList<Cell>();
+	}
+	
 	
 
 }
---
+++ src/simulations/Tuple.java
@@ -0,0 +1,19 @@
+package simulations;
+
+public class Tuple {
+	private final int I;
+	private final int J;
+	
+	public Tuple(int i, int j){
+		this.I = i;
+		this.J = j;
+	}
+
+	public int getIPos(){
+		return I;
+	}
+	
+	public int getJPos(){
+		return J;
+	}
+}
