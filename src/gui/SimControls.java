f800af266ff3d43947e80503aad7d86a1fd25a16
Robert Steilberg <rsteilberg@gmail.com>
2016-09-26 01:48:52 -0400

    refactor Animation, SimControl, and CellNode

--- src/gui/SimControls.java
+++ src/gui/SimControls.java
@@ -10,13 +10,33 @@ import javafx.scene.control.Slider;
 import javafx.scene.layout.Pane;
 import javafx.util.Duration;
 
+/**
+ * 
+ * @author Robert H. Steilberg II | rhs16
+ * 
+ *         The SimControls class creates each GUI element used to control the
+ *         simulation. The elements are drawn to a Pane which is then returned
+ *         to the superclass for adding to the overall Scene. A ComboBox is
+ *         added that changes the simulation to another specified simulation; a
+ *         play button is added that plays the simulation; a step button is
+ *         added that steps through individual frames of the simulation; a pause
+ *         button is added that pauses the simulation; a stop & reset button is
+ *         added that stops the simulation and resets it to its initial state; a
+ *         slider is added that controls the stepping speed of the simulation.
+ *         The SimControls class also adds event handlers to each of these
+ *         objects that give them their functionality.
+ * 
+ *         Dependencies: Animation.java
+ */
 public class SimControls extends Animation {
 	private Timeline myTimeline;
-	private Animation myAnimation;
+	private Animation mySimulation;
+	private ResourceBundle myResources;
 
-	SimControls(Animation currAnimation, Timeline timeline) {
-		myAnimation = currAnimation;
+	SimControls(Animation currAnimation, Timeline timeline, ResourceBundle resources) {
+		mySimulation = currAnimation;
 		myTimeline = timeline;
+		myResources = resources;
 	}
 
 	/**
@@ -32,60 +52,39 @@ public class SimControls extends Animation {
 		double secondDelay = 1.0 / framesPerSecond;
 		myTimeline.stop(); // stop the current run
 		myTimeline.getKeyFrames().clear(); // clear out the old frame
-		KeyFrame frame = new KeyFrame(Duration.millis(millisecondDelay), e -> myAnimation.step(secondDelay));
+		KeyFrame frame = new KeyFrame(Duration.millis(millisecondDelay), e -> mySimulation.step(secondDelay));
 		myTimeline.getKeyFrames().add(frame);
 		myTimeline.play();
 	}
 
 	/**
 	 * Handle click to stop button by stopping the simulation
-	 * 
-	 * @param e
-	 *            the MouseEvent registering the click
-	 * @param t
-	 *            the Timeline that is running through the step function
 	 */
-	private void handleStop(Timeline animation) {
-		animation.stop();
-		myAnimation.resetSimulation();
+	private void handleStop() {
+		mySimulation.resetSimulation(myTimeline);
 	}
 
 	/**
 	 * Handle click to pause button by pausing the simulation
-	 * 
-	 * @param e
-	 *            the MouseEvent registering the click
-	 * @param t
-	 *            the Timeline that is running through the step function
 	 */
-	private void handlePause(Timeline animation) {
-		animation.pause();
+	private void handlePause() {
+		myTimeline.pause();
 	}
 
 	/**
 	 * Handle click to step button by stepping through one cycle of the
 	 * simulation
-	 * 
-	 * @param e
-	 *            the MouseEvent registering the click
-	 * @param t
-	 *            the Timeline that is running through the step function
 	 */
-	private void handleStep(Timeline animation) {
-		animation.pause(); // in case we are already playing
-		myAnimation.step(DEFAULT_SECOND_DELAY);
+	private void handleStep() {
+		myTimeline.pause(); // in case we are already playing
+		mySimulation.step(1000 / Integer.parseInt(myResources.getString("DefaultFPS")));
 	}
 
 	/**
 	 * Handle click to play button by starting the simulation
-	 * 
-	 * @param e
-	 *            the MouseEvent registering the click
-	 * @param t
-	 *            the Timeline that is running through the step function
 	 */
-	private void handlePlay(Timeline animation) {
-		animation.play();
+	private void handlePlay() {
+		myTimeline.play();
 	}
 
 	/**
@@ -105,22 +104,21 @@ public class SimControls extends Animation {
 	}
 
 	/**
-	 * Create the ComboBox to display and change simulations
+	 * Create the ComboBox used to display and change simulations
 	 * 
-	 * @param resources
-	 *            the ResourceBundle containing the properties file
 	 * @return the ComboBox with preset simulation choices
 	 */
 	private ComboBox<String> createComboBox() {
 		ComboBox<String> comboBox = new ComboBox<String>();
 		comboBox.getItems().addAll(myResources.getString("GameOfLifeSim"), myResources.getString("SegregationSim"),
 				myResources.getString("PredatorPreySim"), myResources.getString("FireSim"));
-		comboBox.setValue(myResources.getString("GameOfLifeSim")); // default simulation
+		// set Game of Life as the default simulation
+		comboBox.setValue(myResources.getString("GameOfLifeSim"));
 		comboBox.setMinWidth(Integer.parseInt(myResources.getString("ComboBoxMinWidth")));
 		comboBox.setLayoutX(Integer.parseInt(myResources.getString("ComboBoxXPos")));
 		comboBox.setLayoutY(Integer.parseInt(myResources.getString("ComboBoxYPos")));
-		comboBox.valueProperty().addListener(e -> myAnimation.resetSimulation());
-		myAnimation.myComboBox = comboBox;
+		comboBox.valueProperty().addListener(e -> mySimulation.resetSimulation(myTimeline));
+		mySimulation.myComboBox = comboBox;
 		return comboBox;
 	}
 
@@ -129,29 +127,30 @@ public class SimControls extends Animation {
 	 * 
 	 * @param newButton
 	 *            the button to which an event handler should be set
+	 * 
+	 * @param buttonID
+	 *            a String corresponding to the button's type
 	 */
 	private void setButtonEventHandler(Button newButton, String buttonID) {
 		if (buttonID.equals("PlayButton")) {
-			newButton.setOnMouseClicked(e -> handlePlay(myTimeline));
+			newButton.setOnMouseClicked(e -> handlePlay());
 		}
 		if (buttonID.equals("StepButton")) {
-			newButton.setOnMouseClicked(e -> handleStep(myTimeline));
+			newButton.setOnMouseClicked(e -> handleStep());
 		}
 		if (buttonID.equals("PauseButton")) {
-			newButton.setOnMouseClicked(e -> handlePause(myTimeline));
+			newButton.setOnMouseClicked(e -> handlePause());
 		}
 		if (buttonID.equals("StopButton")) {
-			newButton.setOnMouseClicked(e -> handleStop(myTimeline));
+			newButton.setOnMouseClicked(e -> handleStop());
 		}
 	}
 
 	/**
-	 * Create a button to signal some simulation change
+	 * Create a specified button to signal some simulation change
 	 * 
 	 * @param text
 	 *            The String to be displayed on the button
-	 * @param y
-	 *            The y-coordinate of the button
 	 * @return the button
 	 */
 	private Button createButton(String text) {
@@ -167,10 +166,15 @@ public class SimControls extends Animation {
 	/**
 	 * Create the pane containing all of the simulation controls
 	 * 
+	 * @param controls
+	 *            the pane onto which controls will be placed
+	 * 
 	 * @return the control pane
 	 */
-	public void addControls(Pane controls) {
+	protected void addControls(Pane controls) {
+		// access properties file
 		myResources = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE + LANGUAGE);
+		// set background of GUI
 		controls.setStyle("-fx-background-color: #98a2c5");
 		controls.getChildren().addAll(createComboBox(), createButton("PlayButton"), createButton("StepButton"),
 				createButton("PauseButton"), createButton("StopButton"), createSlider());
