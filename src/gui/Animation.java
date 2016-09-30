4b951896166d498ee277c2465d2cdc4cc6cd05b3
Robert Steilberg <rsteilberg@gmail.com>
2016-09-30 14:10:50 -0400

    create GridParse and SimEvents, refactor inheritance

--- src/gui/Animation.java
+++ src/gui/Animation.java
@@ -3,46 +3,48 @@ package gui;
 import java.util.ResourceBundle;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.shape.Polygon;
 import javafx.util.Duration;
 import simulations.*;
 
 /**
- * 
  * @author Robert H. Steilberg II | rhs16
  * 
  *         The Animation class handles the GUI for the program. After creating
  *         the general Scene, the SimControls class is used to populate the
- *         scene with functional control elements. Then, according to the
- *         default simulation set in the properties file, the grid is
- *         initialized via the simulation's initGrid() method to that
- *         simulation's parameters. The step function is prepared for stepping.
- *         The simulation begins when the play button is clicked. The step
- *         function updates the simulation grid on each frame via the
- *         updateGrid() method, clears the old grid, and then prints the new
- *         one. Each grid iteration is drawn via the CellNode class that creates
- *         each cell according to a specified shape and then returns the cell
- *         with the correct state according to the simulation. The simulation
- *         continues infinitely, or until it is stopped by the user.
+ *         scene with control elements. The SimEvents class adds functionality
+ *         to these elements. Then, according to the default simulation set in
+ *         the properties file, the grid is initialized via GridParser class to
+ *         that simulation's parameters. The step function is prepared for
+ *         stepping, and the simulation begins when the play button is clicked.
+ *         The step function updates the simulation grid on each frame via the
+ *         GridParser class. Each grid iteration is drawn via the CellNode class
+ *         that creates each cell according to a specified shape and then
+ *         returns the cell with the correct state according to the simulation.
+ *         The simulation continues until it is stopped by the user.
  * 
- *         Dependencies: SimControls.java, CellNode.java
+ *         Dependencies: SimControls.java, SimEvents.java, CellNode.java,
+ *         GridParser.java
  */
 public class Animation {
 	private static final String TITLE = "CellSociety";
-	public static final String DEFAULT_RESOURCE_PACKAGE = "resources/";
-	public static final String LANGUAGE = "English";
-
-	private int GRID_SIZE = 500;
+	private static final String DEFAULT_RESOURCE_PACKAGE = "resources/";
+	private static final String LANGUAGE = "English";
+	private static final String STYLESHEET = "style.css";
 	private ResourceBundle myResources;
 	private Pane myRoot;
-	private Timeline myTimeline;
 	private Grid myGrid;
-	protected Simulation mySimulation;
-	public ComboBox<String> myComboBox;
+	private Simulation mySimulation;
+	private Timeline myTimeline;
+	private GridParser myGridParser;
+	// myComboBox is protected because it is instantiated in the SimControls
+	// class when it is created. myComboBox must be available to Animation.java
+	// because its value must be checked each time the user chooses a new
+	// simulation to run.
+	// QUESTION is this explanation okay?
+	protected ComboBox<String> myComboBox;
 
 	/**
 	 * Get the window title for the scene
@@ -61,51 +63,8 @@ public class Animation {
 	 */
 	protected void resetSimulation(Timeline animation) {
 		animation.stop();
-		clearGrid();
+		myGridParser.clearGrid();
 		initStep(myComboBox.getValue());
-	}
-
-	/**
-	 * Clear the grid and re-initialize the simulation
-	 */
-	private void clearGrid() {
-		for (int i = 0; i < mySimulation.getGridHeight(); i++) {
-			for (int j = 0; j < mySimulation.getGridWidth(); j++) {
-				String id = Integer.toString(i) + Integer.toString(j);
-				// get each node via CSS id
-				Node toDelete = myRoot.lookup("#" + id);
-				myRoot.getChildren().remove(toDelete);
-			}
-		}
-	}
-
-	/**
-	 * Draw the grid to the scene
-	 * 
-	 * @param newGrid
-	 *            true if the grid is being drawn for the first time, false
-	 *            otherwise
-	 */
-	private void drawGrid(boolean newGrid) {
-		if (!newGrid) {
-			clearGrid();
-		}
-		double cellSize = GRID_SIZE / mySimulation.getGridHeight();
-		for (int i = 0; i < mySimulation.getGridHeight(); i++) {
-			for (int j = 0; j < mySimulation.getGridWidth(); j++) {
-				int numVertices = 4;
-				CellNode node = new CellNode();
-				Polygon cell = node.getCellNode(myGrid, cellSize, Integer.parseInt(myResources.getString("GridOffset")),
-						i, j, numVertices);
-				if (newGrid) {
-					String id = Integer.toString(i) + Integer.toString(j);
-					// set a CSS id so we can get this cell later to remove it
-					cell.setId(id);
-				}
-				myRoot.getChildren().add(cell);
-			}
-		}
-
 	}
 
 	/**
@@ -137,7 +96,8 @@ public class Animation {
 	 */
 	protected void initStep(String simulation) {
 		setSimulation(simulation);
-		drawGrid(true); // pass true because this is a new grid
+		myGridParser = new GridParser(mySimulation, myGrid, myResources, myRoot);
+		myGridParser.drawGrid(true); // pass true because this is a new grid
 		myTimeline = new Timeline();
 		int framesPerSecond = Integer.parseInt(myResources.getString("DefaultFPS"));
 		KeyFrame frame = new KeyFrame(Duration.millis(1000 / framesPerSecond), e -> step(1.0 / framesPerSecond));
@@ -152,7 +112,8 @@ public class Animation {
 	 */
 	protected void step(double elapsedTime) {
 		mySimulation.updateGrid(); // calculate new grid
-		drawGrid(false); // pass false because we are updating an old grid
+		// pass false because we are updating an old grid
+		myGridParser.drawGrid(false);
 	}
 
 	/**
@@ -167,6 +128,7 @@ public class Animation {
 	public Scene init() {
 		myResources = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE + LANGUAGE);
 		myRoot = new Pane();
+		myRoot.getStylesheets().add(DEFAULT_RESOURCE_PACKAGE + STYLESHEET);
 		initStep(myResources.getString("DefaultSimulation"));
 		SimControls controllers = new SimControls(this, myTimeline, myResources);
 		controllers.addControls(myRoot);
